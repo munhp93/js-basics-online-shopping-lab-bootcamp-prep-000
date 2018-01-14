2f166bc180a65caceb95842a4d729e8ec681ddd7
Hoang Phuc Nguyen <munhp93@gmail.com>
2018-01-14 17:17:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,18 +10,40 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var itemPrice = [Math.random()*100];
- var itemName  = [];
- for (let i=0; i<itemName.length; i++);
- return addToCart.push("${itemName[i]}":"${itemPrice[]}");
+ var price = Math.floor(Math.random() * 100 + 1);
+ cart.push({[item]: price});
+ console.log('${item} has been added to your cart.');
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  const l = cart.length;
+  if (!l){
+      return console.log("Your shopping cart is empty.");
+  }
+  let itemsAndPrices = [];
+  for (let i = 0; i<l; i++){
+    let itemAndPrice = cart[i];
+    let item = Object.keys(itemAndPrice)[0];
+    let price = itemAndPrice[item];
+    itemsAndPrices.push("${item} at \$${price}.");
+  }
+  switch(itemsAndPrices.length) {
+    case 1:
+      break;
+    case 2:
+      itemsAndPrices = itemsAndPrices.join(" and ");
+      break;
+    default:
+      itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
+      itemsAndPrices = itemsAndPrices.join(", ");
+  }
+
+  console.log(`In your cart, you have ${itemsAndPrices}.`);
 }
 
 function total() {
-  // write your code here
+
 }
 
 function removeFromCart(item) {
